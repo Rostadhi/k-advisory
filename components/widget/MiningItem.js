c4e4cdd8fbcf2b7ea612c07de39827c51dfc86b2
Rostadhi <rostadhi@gmail.com>
2021-12-14 01:30:49 +0700

    dfv

--- components/widget/MiningItem.js
+++ components/widget/MiningItem.js
@@ -6,13 +6,13 @@ export default function MiningItem() {
         <div className="flex flex-row py-16 justify-center">
             <div className="w-4/12 text-left">
                 <h3 className="text-blue-700 text-2xl font-semibold">Mining</h3>
-                <p className="text-lg leading-7 pt-6">Coconut Charcoal Briquette is a compact block charcoal which made from coconut shell charcoal. Our charcoal briquette is made of granular coconut shell charcoal that is crushed charcoal and moulded using a natural binder. As fuel, it is hotter and last longer than ordinary charcoal. It is suitable for household use both indoor and outdoor. Also, it can be used in food industries, for barbecue, stove fuel, metallurgy, etc.</p>
+                <p className="text-lg leading-7 pt-6">Telecomunication is one of our expertise, we have accumulated the best practices for the respective industries. Our consultants are knowledgeable of the markets and environment in the respective industries and provide the optimum services through their extensive experience. The benefit of ever functioning in the real sector has enrich us in finding solutions for our patrons.</p>
                 <div className="flex gap-4 mt-14">
                     <a className="border border-gray-300 text-black hover:border-blue-700 hover:text-white hover:bg-blue-700 rounded-md transition p-3 font-semibold text-lg inline-block">See Details</a>
                     <a href="https://www.k-advisory.co.id/" className="border border-gray-300 text-black hover:border-blue-700 hover:text-white hover:bg-blue-700 rounded-md transition p-3 font-semibold text-lg inline-block">Contact Us</a>
                 </div>
             </div>
-            <Image src="https://universalcoco.com/static/media/coconut-briquette-charcoal.6e933f70.png" width="300" height="300" />
+            <Image src="/mining.png" width="300" height="300" />
         </div>
     );
 };
