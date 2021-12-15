ef53fd22dea8909194a05343f202fb1259137806
Rostadhi <rostadhi@gmail.com>
2021-12-15 13:58:45 +0700

    Update ProjectSection.js

--- components/section/ProjectSection.js
+++ components/section/ProjectSection.js
@@ -22,22 +22,46 @@ export default function ProjectSection() {
                     <div className="text-center">
                         <Image src="/coconut.png" width="120" height="120" />
                         <h4 className="text-black text-center text-lg font-semibold pb-8">Corporate Finance</h4>
-                        <p className="text-black text-left text-base font-normal tracking-wider leading-7">Integrity means telling the truth, keeping our word and treating others with fairness and respect. Integrity is one of our most cherished assets. It must not be compromised.</p>
+                        <Link href="/www.ojk.com">
+                            <a
+                                className="border border-black text-blue hover:text-blue-700 hover:bg-white rounded-md transition py-2 px-5 mt-5 font-semibold text-lg inline-block"
+                            >
+                            Click For Details
+                            </a>
+                        </Link>
                     </div>
                     <div className="text-center">
                         <Image src="/coconut.png" width="120" height="120" />
                         <h4 className="text-black text-center text-lg font-semibold pb-8">Capital Budgeting</h4>
-                        <p className="text-black text-left text-base font-normal tracking-wider leading-7">Integrity means telling the truth, keeping our word and treating others with fairness and respect. Integrity is one of our most cherished assets. It must not be compromised.</p>
+                        <Link href="/www.ojk.com">
+                            <a
+                                className="border border-black text-blue hover:text-blue-700 hover:bg-white rounded-md transition py-2 px-5 mt-5 font-semibold text-lg inline-block"
+                            >
+                            Click For Details
+                            </a>
+                        </Link>
                     </div>
                     <div className="text-center">
                         <Image src="/coconut.png" width="120" height="120" />
                         <h4 className="text-black text-center text-lg font-semibold pb-8">Business Process Solution</h4>
-                        <p className="text-black text-left text-base font-normal tracking-wider leading-7">Integrity means telling the truth, keeping our word and treating others with fairness and respect. Integrity is one of our most cherished assets. It must not be compromised.</p>
+                        <Link href="/www.ojk.com">
+                            <a
+                                className="border border-black text-blue hover:text-blue-700 hover:bg-white rounded-md transition py-2 px-5 mt-5 font-semibold text-lg inline-block"
+                            >
+                            Click For Details
+                            </a>
+                        </Link>
                     </div>
                     <div className="text-center">
                         <Image src="/coconut.png" width="120" height="120" />
                         <h4 className="text-black text-center text-lg font-semibold pb-8">Management Exposure</h4>
-                        <p className="text-black text-left text-base font-normal tracking-wider leading-7">Integrity means telling the truth, keeping our word and treating others with fairness and respect. Integrity is one of our most cherished assets. It must not be compromised.</p>
+                        <Link href="/www.ojk.com">
+                            <a
+                                className="border border-black text-blue hover:text-blue-700 hover:bg-white rounded-md transition py-2 px-5 mt-5 font-semibold text-lg inline-block"
+                            >
+                            Click For Details
+                            </a>
+                        </Link>
                     </div>
                 </div>
             </div>
